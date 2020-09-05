5f7ab30badefbeead750a50ae040e8ff76ef43c4
tsuny221 <kingoftsuny@gmail.com>
2020-09-05 12:49:45 +0000

    [Update] sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
+
+  resources :users
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
