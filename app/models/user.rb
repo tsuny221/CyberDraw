1869fbbd52413fd63f8fdddbf5d57a39e23ba8bf
tsuny221 <kingoftsuny@gmail.com>
2020-09-06 03:26:56 +0000

    [Update] form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  enum gender: { 男: 1, 女: 2, その他: 3 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'
-      }
-
+  devise_for :users
   resources :users
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
